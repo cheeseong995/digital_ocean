544f1d9d8ed1d3f444e5f5bf1df58ec4bae460b4
Chee Seong <65995806+cheeseong995@users.noreply.github.com>
2023-04-17 21:39:24 +0800

    Update test_print.py

--- test_print.py
+++ test_print.py
@@ -5,7 +5,10 @@ Created on Mon Apr 17 14:57:33 2023
 @author: CheeSeong
 """
 
-from function_script import add_2_numbers
+try:
+  from function_script import add_2_numbers
 
-theSum = add_2_numbers(2,5)
-print(f'The sum is {theSum}')
+  theSum = add_2_numbers(2,5)
+  print(f'The sum is {theSum}')
+except Exception as e:
+  print(e)
