3cf0bdf56a9bb887614a104860ffb0d56d51be22
Chee Seong <65995806+cheeseong995@users.noreply.github.com>
2023-04-17 15:42:33 +0800

    Update test_print.py

--- test_print.py
+++ test_print.py
@@ -5,4 +5,21 @@ Created on Mon Apr 17 14:57:33 2023
 @author: CheeSeong
 """
 
-print("hello")
+import requests
+
+def send_telegram_message(group_id , send_url , text_type , error_message = ""):
+    
+    date_value = datetime.now() - timedelta(days = 1)
+    date = date_value.strftime("%Y-%m-%d")
+    
+    if text_type == 'failed':
+        text = f"Showroom data tried to update for {date} but failed. Error message: {error_message}"
+    else:
+        text = f"Showroom data have been updated successfully for {date}."
+
+    group_id = group_id
+    send_url = send_url
+
+    requests.post(send_url , json = {'chat_id' : group_id, 'text': text})
+    
+send_telegram_message(-1001763318295 , "https://api.telegram.org/bot5564798304:AAHyN_uuboKonRVhVvVSB9sBe9gfhiViJXk/sendMessage" , "failed" , error_message = "Testing")
