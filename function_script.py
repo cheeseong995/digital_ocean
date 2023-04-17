bf4e38c110cd122d7a171d38b023dfb24c2f29e2
Chee Seong <65995806+cheeseong995@users.noreply.github.com>
2023-04-17 21:37:15 +0800

    Add files via upload

---
+++ function_script.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 17 21:34:21 2023
+
+@author: CheeSeong
+"""
+
+def add_2_numbers(x,y):
+    
+    print(x+y)
+    return x+y
--- test_print.py
+++ test_print.py
@@ -5,25 +5,7 @@ Created on Mon Apr 17 14:57:33 2023
 @author: CheeSeong
 """
 
-import requests
-from datetime import datetime , timedelta
+from function_script import add_2_numbers
 
-def send_telegram_message(group_id , send_url , text_type , error_message = ""):
-    
-    date_value = datetime.now() - timedelta(days = 1)
-    date = date_value.strftime("%Y-%m-%d")
-    
-    if text_type == 'failed':
-        text = f"Showroom data tried to update for {date} but failed. Error message: {error_message}"
-    else:
-        text = f"Showroom data have been updated successfully for {date}."
-
-    group_id = group_id
-    send_url = send_url
-
-    requests.post(send_url , json = {'chat_id' : group_id, 'text': text})
-try:    
-    send_telegram_message(-1001763318295 , "https://api.telegram.org/bot5564798304:AAHyN_uuboKonRVhVvVSB9sBe9gfhiViJXk/sendMessage" , "failed" , error_message = "Testing")
-    print("Yes")
-except Exception as e:
-    print(e)
+theSum = add_2_numbers(2,5)
+print(f'The sum is {theSum}')
